554b83a545e6158d0c89747ef8e75d0d9cb8ea4f
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-10-02 00:05:42 -0500

    add vuex

--- src/components/movies/Movies.vue
+++ src/components/movies/Movies.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1 class="title">Movies</h1>
+  <div>
+    <h1 class="title">Movies</h1>
+    <h2 class="subtitle">{{ message }}</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  computed: {
+    message() {
+      return this.$store.state.hello
+    }
+  }
 }
 </script>
